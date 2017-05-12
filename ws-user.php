1883e4021ce4673c5c51b856a1f7b488d8c18c41
TAH55733 <hatapia@bancopatagonia.com.ar>
2017-05-12 17:50:42 -0300

    login de usuarios

--- ws-user.php
+++ ws-user.php
@@ -11,33 +11,43 @@ $wsUserCommon = new Slim();
 $array_ini = parse_ini_file("config.ini");
 
 $pdo = new PDO("mysql:dbname=u693453499_turno","u693453499_turno", $array_ini['dbpass']);
-$db = new NotORM($pdo);
+$structure = new NotORM_Structure_Convention(
+    $primary = "emt_user_id", // id_$table
+    $foreign = "%s", // id_$table
+    $table = "%s", // {$table}s
+    $prefix = "emt_" // wp_$table
+);
+$db = new NotORM($pdo,$structure);
 
-$wsUserCommon->get("/loginUser/:username/:password", function ($username,$password) use ($wsUserCommon, $db){    
+$wsUserCommon->post("/loginUser", function () use ($wsUserCommon, $db){    
 
     $wsUserCommon->response()->header("Content-Type", "application/json");
     
-    //$user = $wsUserCommon->request()->post();
-    $users = $db->users()->where("username", $username);
-    //->where("password", $password)
+    $data = $wsUserCommon->request()->post();
+    $userToLogin = array(
+        "active" => "1",
+        "username" => $data["username"],
+        "password" => $data["password"]
+    );
+    $users = $db->users()->where($userToLogin);
     if ($user = $users->fetch()) {
-        $jsonResponse []  = array(
-            "id" => $user["id_user"],
-            "username" => $user["username"],
-            "firstname" => $user["firstname"],
-            "lastname" => $user["lastname"],
-            "birthday" => $user["birthday"],
-            "email" => $user["email"],
-            "telephone" => $user["telephone"],
-            "identication_type" => $user["identication_type"],
-            "identication_number" => $user["identication_number"],
-            "state" => $user["state"],
-            "activation_code" => $user["activation_code"]
-        );
+        // $jsonResponse []  = array(
+        //     "id" => $user["id_user"],
+        //     "username" => $user["username"],
+        //     "firstname" => $user["firstname"],
+        //     "lastname" => $user["lastname"],
+        //     "birthday" => $user["birthday"],
+        //     "email" => $user["email"],
+        //     "telephone" => $user["telephone"],
+        //     "identication_type" => $user["identication_type"],
+        //     "identication_number" => $user["identication_number"],
+        //     "state" => $user["state"],
+        //     "activation_code" => $user["activation_code"]
+        // );
         echo json_encode(array(
             "data" => $user["email"],
             "isValid" => true,
-            "status" => "ok",
+            "status" => true,
             "message" => "Usuario Valido"
             ));
     }else{
@@ -52,9 +62,8 @@ $wsUserCommon->get("/loginUser/:username/:password", function ($username,$passwo
 $wsUserCommon->post("/insertUser", function () use ($wsUserCommon, $db){    
 
     $wsUserCommon->response()->header("Content-Type", "application/json");
+    //username=testpost&password=testa
     $user = $wsUserCommon->request()->post();
-    // $user = $wsUserCommon->request()->post();
-    // echo json_encode($user);
     $result = $db->emt_users()->insert($user);
     echo json_encode(array(
             "status" => true,
@@ -67,22 +76,28 @@ $wsUserCommon->post("/insertUser", function () use ($wsUserCommon, $db){
 $wsUserCommon->get("/deleteUser/:id", function ($id) use ($wsUserCommon, $db){    
 
     $wsUserCommon->response()->header("Content-Type", "application/json");
-    
-    $users = $db->emt_users()->where("emt_user_id", $id);
-    if ($users->fetch()) {
-        $result = $users->delete();
-        echo json_encode($result);
-
-        echo json_encode(array(
+    $unactivate = array(
+        "active" => "0"
+    );
+    $user = $db->users()->where("emt_user_id", $id)->fetch();
+ if ($user !== false) {
+        $result = $user->update($unactivate);
+        if($result !== false && $result !== 0){
+            echo json_encode(array(
             "status" => true,
-            "message" => "Registro borrado exitosamente"
-        ));
-    }
-    else{
+            "message" => "Registro Borrado, con el id $id "
+            ));
+        }else{
+            echo json_encode(array(
+            "status" => false,
+            "message" => "registro no borrado o ya estaba borrado"
+            ));
+        }
+    }else{
         echo json_encode(array(
             "status" => false,
-            "message" => "El Registro con id $id no existe"
-        ));
+            "message" => "No existe un registro con el id $id "
+            ));
     }
 
 });
@@ -91,7 +106,7 @@ $wsUserCommon->get("/getByID/:id", function ($id) use ($wsUserCommon, $db){
 
     $wsUserCommon->response()->header("Content-Type", "application/json");
     
-    $users = $db->emt_users()->where("emt_user_id", $id);
+    $users = $db->users()->where("emt_user_id", $id);
     if ($user = $users->fetch()) {
         $jsonResponse []  = array(
             "id" => $user["emt_user_id"],
@@ -114,7 +129,7 @@ $wsUserCommon->get("/getAll", function () use ($wsUserCommon, $db){
     $wsUserCommon->response()->header("Content-Type", "application/json");
     
     $jsonResponse = array();
-    foreach ($db->emt_users() as $user) {
+    foreach ($db->users()->where("active", "1") as $user) {
         $jsonResponse []  = array(
             "id" => $user["emt_user_id"],
             "username" => $user["username"]
