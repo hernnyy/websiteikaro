a12bdd8a9312aedb7208c6d54e8ce4e246e219e8
hernnyy <jonhnick15@gmail.com>
2017-05-11 23:04:14 -0300

    nueva url

--- ws-common.php
+++ ws-common.php
@@ -20,7 +20,7 @@ $wsCommon->get("/getCustomUrlIkaro/:id", function ($id) use ($wsCommon, $db){
             "status" => true,
             "code" => 200,
             "url_custom" => "ikaroira.com",
-            "message" => "Registro borrado exitosamente"
+            "message" => "url"
             ));
         break;
         case 2:
@@ -28,7 +28,7 @@ $wsCommon->get("/getCustomUrlIkaro/:id", function ($id) use ($wsCommon, $db){
             "status" => true,
             "code" => 200,
             "url_custom" => "www.ikaroira.com",
-            "message" => "Registro borrado exitosamente"
+            "message" => "url 2"
             ));
         break;
         case 3:
@@ -36,20 +36,56 @@ $wsCommon->get("/getCustomUrlIkaro/:id", function ($id) use ($wsCommon, $db){
             "status" => true,
             "code" => 200,
             "url_custom" => "http://www.ikaroira.com",
-            "message" => "Registro borrado exitosamente"
+            "message" => "url completa"
             ));
         break;
         default:
         echo json_encode(array(
             "status" => false,
-            "error" => true,
             "code" => 401,
             "message" => "La URL solicitada No existe"
             ));
     }
 
 });
+$wsCommon->get("/getCustomUrlINCAA/:id", function ($id) use ($wsCommon, $db){    
 
+    $wsCommon->response()->header("Content-Type", "application/json; charset=utf-8");
+    
+    switch($id){
+        case 1:
+            echo json_encode(array(
+            "status" => true,
+            "code" => 200,
+            "url_custom" => "ikaroira.com",
+            "message" => "url simple"
+            ));
+        break;
+        case 2:
+            echo json_encode(array(
+            "status" => true,
+            "code" => 200,
+            "url_custom" => "www.ikaroira.com",
+            "message" => "url www"
+            ));
+        break;
+        case 3:
+            echo json_encode(array(
+            "status" => true,
+            "code" => 200,
+            "url_custom" => "http://www.ikaroira.com",
+            "message" => "url completa"
+            ));
+        break;
+        default:
+        echo json_encode(array(
+            "status" => false,
+            "code" => 401,
+            "message" => "La URL solicitada No existe"
+            ));
+    }
+
+});
 
 // corremos la aplicación
 $wsCommon->run();
